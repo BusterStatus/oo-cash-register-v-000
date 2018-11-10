9dbfeeb035ba5bd0e04765d2c200e665d86e5274
matthew.serre <matthew.serre@gmail.com>
2018-11-10 01:40:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item,price,quantity = 1)
+    @cart = []
+    quantity.times {@cart << item}
+    quantity.times {@total += price}
+  end
+  
+  def apply_discount
+    discounted_total = self.total * ((100 - @discount) / 100)
+  end
+  
+end
